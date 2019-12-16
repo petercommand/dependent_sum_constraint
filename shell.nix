e3b56fd4f65f12d9757864b4bc66666dba55cbd4
petercommand <petercommand@gmail.com>
2019-12-16 22:11:09 +0800

    update build system

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ with pkgs;
 stdenvNoCC.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
+  inherit (pkgs) AgdaStdlib;
   buildInputs = import ./build_input.nix { inherit pkgs; };
 }
 
